40202266fc5e11f4cd435f2875b71af2d6befdd7
Gitcatmeoww <queenie.lin0323@gmail.com>
2023-04-10 15:13:06 -0700

    fix indent issue in tests

--- tests.py
+++ tests.py
@@ -7,12 +7,11 @@ from app import app
 class TestGoTravel(unittest.TestCase):
     baseURL = "http://127.0.0.1:5050"
 
-
-def test_get_recommend(self):
-    response = requests.get(self.baseURL + "/recommend")
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(json.loads(response.content.decode()), {
-                     "status": "success", "response": "test recommend"})
+    def test_get_recommend(self):
+        response = requests.get(self.baseURL + "/recommend")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content.decode()), {
+            "status": "success", "response": "test recommend"})
 
 
 if __name__ == "__main__":
