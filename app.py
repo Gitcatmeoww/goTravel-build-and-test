5ce2e9980293fcd00c930b4049b59b2ffdd7e98d
Gitcatmeoww <queenie.lin0323@gmail.com>
2023-04-19 20:35:02 -0700

    add database config and test

--- app.py
+++ app.py
@@ -1,6 +1,62 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import database_exists, create_database
 
+
+# Database URI
+database_uri = 'postgresql://postgres:postgres@localhost/datawarehouse'
+
+
+# Create database
+def create_datawarehouse(database_uri):
+    if not database_exists(database_uri):
+        create_database(database_uri)
+        print("Database created successfully!")
+    else:
+        print("Database already exists!")
+
+
+# Flask and postgre database configuration
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+create_datawarehouse(database_uri)
+
+
+# Database schema initialization
+class Wishlist(db.Model):
+    __tablename__ = 'wishlist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(20), nullable=False)
+    planned_date = db.Column(db.Date, nullable=False)
+    weather_forecast = db.Column(db.String(200))
+    recommendation = db.Column(db.String(200))
+
+    def __init__(self, destination, planned_date, weather_forecast, recommendation):
+        self.destination = destination
+        self.planned_date = planned_date
+        self.weather_forecast = weather_forecast
+        self.recommendation = recommendation
+
+    def __repr__(self):
+        return f'<Wishlist {self.destination}>'
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'destination': self.destination,
+            'planned_date': self.planned_date,
+            'weather_forecast': self.weather_forecast,
+            'recommendation': self.recommendation
+        }
+
+
+# Initialize table in database
+@app.cli.command('create-db')
+def create_db():
+    db.create_all()
 
 
 @app.route('/recommend', methods=['GET'])
--- tests.py
+++ tests.py
@@ -1,14 +1,36 @@
 import unittest
-import requests
 import json
-from app import app
+from app import app, db, Wishlist
 
 
 class TestGoTravel(unittest.TestCase):
-    baseURL = "http://127.0.0.1:5050"
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/datawarehouse_test'
+        cls.client = app.test_client()
+        db.create_all()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        db.drop_all()
+
+    # Wishlist database should be created properly
+    def test_database_setup(self):
+        test_record_add = Wishlist(
+            destination="berkeley", planned_date="04/18/2023")
+        db.session.add(test_record_add)
+        db.session.commit()
+
+        test_record_get = Wishlist.query.filter_by(
+            destination="berkeley").first()
+        self.assertEqual(test_record_get.destination, "berkeley")
+        self.assertEqual(test_record_get.planned_date, "04/18/2023")
+
+    # Recommend endpoint should return {"status": "success", "response": "test recommend"} with a status code 200
     def test_get_recommend(self):
-        response = requests.get(self.baseURL + "/recommend")
+        response = self.client.get('/recommend')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content.decode()), {
             "status": "success", "response": "test recommend"})
